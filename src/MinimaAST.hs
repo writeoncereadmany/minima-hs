12833431490059fde5d4e47859080d52ed0e16ad
tom <tarjohnson1979@gmail.com>
2017-09-17 16:17:27 +0100

    Add a catamorphism for the AST. Not quite there yet as it doesn't handle context appropriately, but it's a start

--- src/MinimaAST.hs
+++ src/MinimaAST.hs
@@ -13,3 +13,26 @@ data Expression
   | Object [(String, Expression)]
   | Group [Expression]
   deriving (Show)
+
+data ExpressionSemantics t = ExpressionSemantics {
+  foldVariable :: String -> t,
+  foldDeclaration :: String -> t -> t,
+  foldStringLiteral :: String -> t,
+  foldNumberLiteral :: Double -> t,
+  foldCall :: t -> [t] -> t,
+  foldFunction :: [String] -> Expression -> t,
+  foldAccess :: t -> String -> t,
+  foldObject :: [(String, t)] -> t,
+  foldGroup :: [t] -> t
+}
+
+foldExpression :: ExpressionSemantics t -> Expression -> t
+foldExpression with = foldOver where
+  foldOver (Variable name) = foldVariable with name
+  foldOver (Declaration name value) = foldDeclaration with name (foldOver value)
+  foldOver (StringLiteral text) = foldStringLiteral with text
+  foldOver (NumberLiteral number) = foldNumberLiteral with number
+  foldOver (Call func args) = foldCall with (foldOver func) (foldOver <$> args)
+  foldOver (Function params body) = foldFunction with params body
+  foldOver (Access object field) = foldAccess with (foldOver object) field
+  foldOver (Group expressions) = foldGroup with (foldOver <$> expressions)
