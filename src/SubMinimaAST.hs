8c7821c3f6e3eeb9a5f4395bd8b1f24ff98d2504
tomj <tarjohnson1979@gmail.com>
2017-09-18 17:35:11 +0100

    Ok, so it looks like I need to rethink values/context here. IO definitely belongs in context under current schema

--- src/SubMinimaAST.hs
+++ src/SubMinimaAST.hs
@@ -11,19 +11,19 @@ data SubExpression
   -- | SubFunction [String] SubExpression
   -- | SubAccess SubExpression String
   -- | SubObject [(String, SubExpression)]
-  -- | SubGroup [SubExpression]
+  | SubGroup [SubExpression]
   deriving (Show)
 
 data SubExpressionSemantics t c = SubExpressionSemantics {
   foldStringLiteral :: c -> String -> (t, c),
-  foldNumberLiteral :: c -> Double -> (t, c)
+  foldNumberLiteral :: c -> Double -> (t, c),
   -- foldVariable :: c -> String -> (t, c),
   -- foldDeclaration :: c -> String -> t -> (t, c),
   -- foldCall :: c -> t -> [t] -> (t, c),
   -- foldFunction :: c -> [String] -> SubExpression -> (t, c),
   -- foldAccess :: c -> t -> String -> (t, c),
   -- foldObject :: c -> [(String, t)] -> (t, c),
-  -- foldGroup :: c -> [t] -> (t, c)
+  foldGroup :: c -> ([t], c) -> (t, c)
 }
 
 foldSubExpression :: SubExpressionSemantics t c -> c -> SubExpression -> (t, c)
@@ -36,14 +36,14 @@ foldSubExpression with = foldOver where
   -- foldOver context (SubFunction params body) = foldFunction with context params body
   -- foldOver context (SubAccess object field) = foldAccess with context (fst $ foldOver context object) field
   -- foldOver context (SubObject fields) = foldObject with context (map (\(n, x) -> (n, fst $ foldOver context x)) fields)
-  -- foldOver context (SubGroup expressions) = foldGroup with context (case expressions of
-  --     (x:xs) -> fst $ foldGroup' (foldOver context x) xs foldOver where
-  --       foldGroup' (initial, context) = foldGroup'' ([initial], context) where
-  --         foldGroup'' (acc, context) (x:xs) folder = let (next, c) = folder context x
-  --                                                     in foldGroup'' (acc ++ [next], c) xs folder
-  --         foldGroup'' (acc, context) [] folder = (acc, context)
-  --     [] -> error "Cannot have an empty group"
-  --   )
+  foldOver context (SubGroup expressions) = foldGroup with context (case expressions of
+      (x:xs) -> foldGroup' (foldOver context x) xs foldOver where
+        foldGroup' (initial, context) = foldGroup'' ([initial], context) where
+          foldGroup'' (acc, context) (x:xs) folder = let (next, c) = folder context x
+                                                      in foldGroup'' (acc ++ [next], c) xs folder
+          foldGroup'' (acc, context) [] folder = (acc, context)
+      [] -> error "Cannot have an empty group"
+    )
 
 contextFree :: (a -> b) -> c -> a -> (b, c)
 contextFree f = \context -> \initial -> (f initial, context)
--- src/SubMinimaPrettyPrinter.hs
+++ src/SubMinimaPrettyPrinter.hs
@@ -1,19 +1,25 @@
 module SubMinimaPrettyPrinter where
 
 import SubMinimaAST
+import Data.List
 
-prettyPrinter :: SubExpressionSemantics String ()
-prettyPrinter = SubExpressionSemantics {
-  foldStringLiteral = contextFree id,
-  foldNumberLiteral = contextFree show
-}
+testAST :: SubExpression
+testAST = SubGroup [ SubStringLiteral "hello", SubNumberLiteral 42, SubStringLiteral "cheese" ]
 
-debuggingPrettyPrinter :: SubExpressionSemantics (IO String) ()
+-- prettyPrinter :: SubExpressionSemantics String ()
+-- prettyPrinter = SubExpressionSemantics {
+--   foldStringLiteral = contextFree id,
+--   foldNumberLiteral = contextFree show,
+--   foldGroup = contextFree (intercalate ", ")
+-- }
+
+debuggingPrettyPrinter :: SubExpressionSemantics String (IO ())
 debuggingPrettyPrinter = SubExpressionSemantics {
-  foldStringLiteral = contextFree (\text -> do { putStrLn ("Folding string " ++ text); return text }),
-  foldNumberLiteral = contextFree (\num -> do { putStrLn ("Folding number " ++ show num); return (show num)})
+  foldStringLiteral = \c -> \t -> (t, c >> putStrLn "folding string"),
+  foldNumberLiteral = \c -> \n -> (show n, c >> putStrLn "folding number"),
+  foldGroup = \c -> \(exps, newC) -> (last exps, newC)
 }
 
 -- note that we need to have a top-level IO something, so we need to extract that part of the tuple
-debug :: SubExpression -> IO String
-debug subexp = fst $ foldSubExpression debuggingPrettyPrinter () subexp
+debug :: SubExpression -> IO ()
+debug subexp = snd $ foldSubExpression debuggingPrettyPrinter (return ()) subexp
