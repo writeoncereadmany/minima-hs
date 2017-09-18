29decb3f5fe94e71051361dec0329565114e897b
tomj <tarjohnson1979@gmail.com>
2017-09-18 17:56:06 +0100

    Work in progress: does not currently build. Moving towards a more principled way of dealing with ASTs

--- src/MinimaAST.hs
+++ src/MinimaAST.hs
@@ -14,45 +14,28 @@ data Expression
   | Group [Expression]
   deriving (Show)
 
-data ExpressionSemantics t c = ExpressionSemantics {
-  foldVariable :: c -> String -> (t, c),
-  foldDeclaration :: c -> String -> t -> (t, c),
-  foldStringLiteral :: c -> String -> (t, c),
-  foldNumberLiteral :: c -> Double -> (t, c),
-  foldCall :: c -> t -> [t] -> (t, c),
-  foldFunction :: c -> [String] -> Expression -> (t, c),
-  foldAccess :: c -> t -> String -> (t, c),
-  foldObject :: c -> [(String, t)] -> (t, c),
-  foldGroup :: c -> [t] -> (t, c)
+data ExpressionSemantics c = ExpressionSemantics {
+  foldVariable :: c -> String -> c,
+  foldDeclaration :: c -> String -> c -> c,
+  foldStringLiteral :: c -> String -> c,
+  foldNumberLiteral :: c -> Double -> c,
+  foldCall :: c -> c -> [c] -> c,
+  foldFunction :: c -> [String] -> Expression -> c,
+  foldAccess :: c -> c -> String -> c,
+  foldObject :: c -> [(String, c)] -> c,
+  foldGroup :: c -> [c] -> c
 }
 
-foldExpression :: ExpressionSemantics t c -> c -> Expression -> (t, c)
+foldExpression :: ExpressionSemantics c -> c -> Expression -> c
 foldExpression with = foldOver where
   foldOver context (Variable name) = foldVariable with context name
-  foldOver context (Declaration name value) = foldDeclaration with context name (fst $ foldOver context value)
+  foldOver context (Declaration name value) = foldDeclaration with context name (foldOver context value)
   foldOver context (StringLiteral text) = foldStringLiteral with context text
   foldOver context (NumberLiteral number) = foldNumberLiteral with context number
-  foldOver context (Call func args) = foldCall with context (fst $ foldOver context func) (fst <$> ((foldOver context) <$> args))
+  foldOver context (Call func args) = foldCall with context (foldOver context func) ((foldOver context) <$> args)
   foldOver context (Function params body) = foldFunction with context params body
-  foldOver context (Access object field) = foldAccess with context (fst $ foldOver context object) field
-  foldOver context (Object fields) = foldObject with context (map (\(n, x) -> (n, fst $ foldOver context x)) fields)
-  foldOver context (Group expressions) = foldGroup with context (case expressions of
-      (x:xs) -> fst $ foldGroup' (foldOver context x) xs foldOver where
-        foldGroup' (initial, context) = foldGroup'' ([initial], context) where
-          foldGroup'' (acc, context) (x:xs) folder = let (next, c) = folder context x
-                                                      in foldGroup'' (acc ++ [next], c) xs folder
-          foldGroup'' (acc, context) [] folder = (acc, context)
-      [] -> error "Cannot have an empty group"
-    )
-
-contextFree :: (a -> b) -> c -> a -> (b, c)
-contextFree f = \context -> \initial -> (f initial, context)
-
-contextFree2 :: (x -> y -> z) -> c -> x -> y -> (z, c)
-contextFree2 f = \context -> \x -> \y -> (f x y, context)
-
-usingContext :: (c -> a -> b) -> c -> a -> (b, c)
-usingContext f = \context -> \initial -> (f context initial, context)
-
-usingContext2 :: (c -> x -> y -> z) -> c -> x -> y -> (z, c)
-usingContext2 f = \context -> \x -> \y -> (f context x y, context)
+  foldOver context (Access object field) = foldAccess with context (foldOver context object) field
+  foldOver context (Object fields) = foldObject with context (map (\(n, x) -> (n, foldOver context x)) fields)
+  foldOver context (Group expressions) = foldGroup with context (build context expressions []) where
+    build context [] acc = acc
+    build context (x:xs) acc = let newContext = foldOver context x in build newContext xs (newContext : acc)
